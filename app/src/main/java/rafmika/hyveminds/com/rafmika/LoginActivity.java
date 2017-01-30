419ba881f0db3647be78f95ab0394c7dc6d6ef44
edward121 <edward.petersen@yandex.com>
2017-01-30 17:25:15 +0100

    fix Login MD5 function

--- app/src/main/java/rafmika/hyveminds/com/rafmika/LoginActivity.java
+++ app/src/main/java/rafmika/hyveminds/com/rafmika/LoginActivity.java
@@ -111,17 +111,23 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-    public String md5(String s) {
+    public String md5(final String s) {
+        final String MD5 = "MD5";
         try {
             // Create MD5 Hash
-            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
             digest.update(s.getBytes());
             byte messageDigest[] = digest.digest();
 
             // Create Hex String
-            StringBuffer hexString = new StringBuffer();
-            for (int i=0; i<messageDigest.length; i++)
-                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
             return hexString.toString();
 
         } catch (NoSuchAlgorithmException e) {
